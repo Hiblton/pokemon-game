b359a6c0610f1627ac005ad20cd4128e422d9fb2
Slava Selivonchyk <slava.selivonchyk@gmail.com>
2021-01-28 19:26:42 +0100

    Homework 2
    Thanks for the review ;)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Layout from "./components/layout/Layout";
+import Footer from "./components/footer/Footer";
+
+import BgUrl from "./assets/bg3.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, ReactJS!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header title="This is title" descr="This is Description!" />
+      <Layout
+        id="1"
+        title="This is title"
+        descr="This is Description!"
+        urlBg={BgUrl}
+      />
+      <Layout
+        id="2"
+        title="This is title"
+        descr="This is Description!"
+        colorBg="antiquewhite"
+      />
+      <Layout
+        id="3"
+        title="This is title"
+        descr="This is Description!"
+        urlBg={BgUrl}
+      />
+      <Footer />
+    </>
   );
 }
 
